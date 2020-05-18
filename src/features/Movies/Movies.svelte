546f7cf3149a08cb213290da540f04fceeeca371
Victor Radael <victorradael@hotmail.com>
2020-05-18 19:28:15 -0300

    :rocket: App Svelte Search Movies

---
+++ src/features/Movies/Movies.svelte
@@ -0,0 +1,17 @@
+<script>
+  import MovieSearch from "./MovieSearch.svelte";
+  import MovieList from "./MovieList.svelte";
+</script>
+
+<main>
+  <div class="container mx-auto">
+    <div class="md:mx-4">
+      <div data-testid="movie-search">
+        <MovieSearch />
+      </div>
+      <div data-testid="movie-list">
+        <MovieList />
+      </div>
+    </div>
+  </div>
+</main>
